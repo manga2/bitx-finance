56f9e3b3f123da32679a57d3d9b8fef52d84b930
manga2 <codingexpert1001@gmail.com>
2022-04-05 20:13:14 -0700

    mainnet connected

--- src/config.tsx
+++ src/config.tsx
@@ -1,10 +1,10 @@
-export const ENVIRONMENT = 'testnet';
+export const ENVIRONMENT = 'mainnet';
 
 export const dAppName = 'BitX Staking';
 export const SOCIAL_WEBSITE_URL = 'https://bitxfinance.online/';
 
-export const BTX2BTX_CONTRACT_ADDRESS = 'erd1qqqqqqqqqqqqqpgqj26nrxw355c2zf2ty2qmj7r9h2w5ke0pj9qswt3l3p';
+export const BTX2BTX_CONTRACT_ADDRESS = 'erd1qqqqqqqqqqqqqpgqdydrqza459y8qppzj3mz22sd3ak9u5z43qmqek9z57';
 export const BTX2BTX_CONTRACT_ABI = 'abi/bitx-staking.abi.json';
 export const BTX2BTX_CONTRACT_NAME = 'BitxStake';
 export const BTX_TOKEN_NAME = 'BTX';
-export const BTX_TOKEN_ID = 'BTX-c6d295';
+export const BTX_TOKEN_ID = 'BTX-48d004';
