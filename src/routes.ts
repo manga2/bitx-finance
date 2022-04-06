fdf4125cac2bf8ed98f8578b2b0a6f77b5b48e43
manga2 <codingexpert1001@gmail.com>
2022-04-06 07:57:47 -0700

    reset

--- src/routes.ts
+++ src/routes.ts
@@ -4,9 +4,6 @@ import Staking from './pages/Staking';
 import UnlockPage from './pages/UnlockPage';
 
 export const routeNames = {
-  // home: '/',
-  dashboard: '/dashboard',
-  transaction: '/transaction',
   unlock: '/unlock',
   ledger: '/ledger',
   walletconnect: '/walletconnect',
@@ -14,27 +11,6 @@ export const routeNames = {
 };
 
 const routes: Array<any> = [
-  // {
-  //   path: routeNames.home,
-  //   title: 'Home',
-  //   component: Home
-  // },
-  // {
-  //   path: routeNames.dashboard,
-  //   title: 'Dashboard',
-  //   component: Dashboard,
-  //   authenticatedRoute: true
-  // },
-  // {
-  //   path: routeNames.transaction,
-  //   title: 'Transaction',
-  //   component: Transaction
-  // },
-  {
-    path: routeNames.unlock,
-    title: 'Unlock',
-    component: UnlockPage
-  },
   {
     path: routeNames.staking,
     title: 'Staking',
