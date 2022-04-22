5d08fbad89fc4fbfbe35c39e82850cbc8423768b
manga2 <codingexpert1001@gmail.com>
2022-04-21 20:36:19 -0700

    add nft mint abi

--- src/config.tsx
+++ src/config.tsx
@@ -1,38 +1,43 @@
-export const ENVIRONMENT = 'mainnet';
+export const ENVIRONMENT = 'devnet';
 
 export const dAppName = 'BitX Staking';
 export const SOCIAL_WEBSITE_URL = 'https://bitxfinance.online/';
 
 // BTX Pool
-export const BTX2BTX_CONTRACT_ADDRESS = 'erd1qqqqqqqqqqqqqpgqdydrqza459y8qppzj3mz22sd3ak9u5z43qmqek9z57';
+export const BTX2BTX_CONTRACT_ADDRESS = 'erd1qqqqqqqqqqqqqpgqltgyu0kcwkfth9vyquv9s4dgmrx550rjj9qs258v5r';
 export const BTX2BTX_CONTRACT_ABI = 'abi/btx2btx.abi.json';
 export const BTX2BTX_CONTRACT_NAME = 'BitxStake';
 export const BTX_TOKEN_TICKER = 'BTX';
 export const BTX_TOKEN_ID = 'BTX-48d004';
 
-export const BTX2MEX_CONTRACT_ADDRESS = 'erd1qqqqqqqqqqqqqpgqlfmj246rxrnl9sxx5fvxrxh2jpefa27r3qmqt76ey7';
+export const BTX2MEX_CONTRACT_ADDRESS = 'erd1qqqqqqqqqqqqqpgqltgyu0kcwkfth9vyquv9s4dgmrx550rjj9qs258v5r';
 export const BTX2MEX_CONTRACT_ABI = 'abi/btx2mex.abi.json';
 export const BTX2MEX_CONTRACT_NAME = 'BitxStake';
 export const MEX_TOKEN_TICKER = 'MEX';
 export const MEX_TOKEN_ID = 'MEX-455c57';
 
 // DICE Pool
-export const DICE2DICE_CONTRACT_ADDRESS = 'erd1qqqqqqqqqqqqqpgqutcvq6qjfsg95n27pyskkdl98072ct5l3qmqmycd6e';
+export const DICE2DICE_CONTRACT_ADDRESS = 'erd1qqqqqqqqqqqqqpgqltgyu0kcwkfth9vyquv9s4dgmrx550rjj9qs258v5r';
 export const DICE2DICE_CONTRACT_ABI = 'abi/dice2dice.abi.json';
 export const DICE2DICE_CONTRACT_NAME = 'BitxStake';
 export const DICE_TOKEN_TICKER = 'DICE';
 export const DICE_TOKEN_ID = 'DICE-9749cc';
 export const DICE_TOKEN_DECIMALS = 6;
 
-export const BTX2DICE_CONTRACT_ADDRESS = 'erd1qqqqqqqqqqqqqpgqzu589zvpeymkyjzs9l7cz7drk8tafzcl3qmqu5e0fe';
+export const BTX2DICE_CONTRACT_ADDRESS = 'erd1qqqqqqqqqqqqqpgqltgyu0kcwkfth9vyquv9s4dgmrx550rjj9qs258v5r';
 export const BTX2DICE_CONTRACT_ABI = 'abi/btx2dice.abi.json';
 export const BTX2DICE_CONTRACT_NAME = 'BitxStake';
 
 // Pre Sale
-export const CONTRACT_ADDRESS = 'erd1qqqqqqqqqqqqqpgqguy27dwjmghs90vwanm0y054s5yaf0u03qmqasfjjy';
+export const CONTRACT_ADDRESS = 'erd1qqqqqqqqqqqqqpgq32gr5nx4460zwf2evyy6apm4szectdylj9qswkvlp5';
 export const CONTRACT_ABI_URL = '/abi/bitxfinance-ico.abi.json';
 export const CONTRACT_NAME = 'BitxfinanceIco';
 
 export const EXCHANGE_RATE = 0.0005;
 export const MIN_BUY_LIMIT = 1;
 export const MAX_BUY_LIMIT = 3;
+
+// NFT Mint
+export const NFT_CONTRACT_ADDRESS = 'erd1qqqqqqqqqqqqqpgqtg2t4v96ckv7ckketpcgpwdrn4z3f2ugj9qs3xnjp2';
+export const NFT_CONTRACT_ABI_URL = '/abi/bitx-pass-nft-mint.abi.json';
+export const NFT_CONTRACT_NAME = 'BitxPassNftMint';
