a4d8622210c6daea22a4b7fdd0777f127f038c28
manga2 <codingexpert1001@gmail.com>
2022-05-07 07:20:34 -0700

    testnet

--- src/config.tsx
+++ src/config.tsx
@@ -82,8 +82,103 @@
 // export const BTX2HETO_CONTRACT_ABI = 'abi/btx2dice.abi.json';
 // export const BTX2HETO_CONTRACT_NAME = 'BitxStake';
 
+// ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+// export const ENVIRONMENT = 'devnet';
+
+// export const dAppName = 'BitX Platform';
+// export const SOCIAL_WEBSITE_URL = 'https://bitxfinance.online/';
+
+// // BTX Pool
+// export const BTX2BTX_CONTRACT_ADDRESS = 'erd1qqqqqqqqqqqqqpgqltgyu0kcwkfth9vyquv9s4dgmrx550rjj9qs258v5r';
+// export const BTX2BTX_CONTRACT_ABI = 'abi/btx2btx.abi.json';
+// export const BTX2BTX_CONTRACT_NAME = 'BitxStake';
+// export const BTX_TOKEN_TICKER = 'BTX';
+// export const BTX_TOKEN_ID = 'BTX-48d004';
+// export const BTX_TOKEN_DECIMALS = 18;
+
+// export const BTX2MEX_CONTRACT_ADDRESS = 'erd1qqqqqqqqqqqqqpgq7r9n9u389xr23vqyye8maaetcg2r886vj9qsj7sl4k';
+// export const BTX2MEX_CONTRACT_ABI = 'abi/btx2mex.abi.json';
+// export const BTX2MEX_CONTRACT_NAME = 'BitxStake';
+// export const MEX_TOKEN_TICKER = 'MEX';
+// export const MEX_TOKEN_ID = 'MEX-455c57';
+
+// // DICE Pool
+// export const DICE2DICE_CONTRACT_ADDRESS = 'erd1qqqqqqqqqqqqqpgqltgyu0kcwkfth9vyquv9s4dgmrx550rjj9qs258v5r';
+// export const DICE2DICE_CONTRACT_ABI = 'abi/dice2dice.abi.json';
+// export const DICE2DICE_CONTRACT_NAME = 'BitxStake';
+// export const DICE_TOKEN_TICKER = 'DICE';
+// export const DICE_TOKEN_ID = 'DICE-9749cc';
+// export const DICE_TOKEN_DECIMALS = 6;
+
+// export const BTX2DICE_CONTRACT_ADDRESS = 'erd1qqqqqqqqqqqqqpgq7r9n9u389xr23vqyye8maaetcg2r886vj9qsj7sl4k';
+// export const BTX2DICE_CONTRACT_ABI = 'abi/btx2dice.abi.json';
+// export const BTX2DICE_CONTRACT_NAME = 'BitxStake';
+
+// // Pre Sale
+// export const PRESALE_CONTRACT_ADDRESS = 'erd1qqqqqqqqqqqqqpgq32gr5nx4460zwf2evyy6apm4szectdylj9qswkvlp5';
+// export const PRESALE_CONTRACT_ABI_URL = '/abi/bitxfinance-ico.abi.json';
+// export const PRESALE_CONTRACT_NAME = 'BitxfinanceIco';
+
+// export const EXCHANGE_RATE = 0.0005;
+// export const MIN_BUY_LIMIT = 1;
+// export const MAX_BUY_LIMIT = 3;
+// export const PRESALE_WHITELIST_URL = 'https://t.co/9Ek1o08pzx';
+
+// // NFT Mint
+// export const NFT_CONTRACT_ADDRESS = 'erd1qqqqqqqqqqqqqpgqmf59pk39hzelmsvg7gpz60qvt8p8vvkej9qsj6ax3y';
+// export const NFT_CONTRACT_ABI_URL = '/abi/bitx-pass-nft-mint.abi.json';
+// export const NFT_CONTRACT_NAME = 'BitxPassNftMint';
+
+// export const NFT_CARDS = [
+//     {
+//         name: 'GOLD',
+//         price: 2,
+//     },
+//     {
+//         name: 'SILVER',
+//         price: 1,
+//     },
+//     {
+//         name: 'BRONZE',
+//         price: 0.5,
+//     },
+// ];
+
+// // Mare Pool
+// export const MARE2MARE_CONTRACT_ADDRESS = 'erd1qqqqqqqqqqqqqpgqltgyu0kcwkfth9vyquv9s4dgmrx550rjj9qs258v5r';
+// export const MARE2MARE_CONTRACT_ABI = 'abi/dice2dice.abi.json';
+// export const MARE2MARE_CONTRACT_NAME = 'BitxStake';
+// export const MARE_TOKEN_TICKER = 'MARE';
+// export const MARE_TOKEN_ID = 'MARE-63e515';
+// export const MARE_TOKEN_DECIMALS = 6;
+
+// export const BTX2MARE_CONTRACT_ADDRESS = 'erd1qqqqqqqqqqqqqpgq7r9n9u389xr23vqyye8maaetcg2r886vj9qsj7sl4k';
+// export const BTX2MARE_CONTRACT_ABI = 'abi/btx2dice.abi.json';
+// export const BTX2MARE_CONTRACT_NAME = 'BitxStake';
+
+// // Heto Pool
+// export const HETO2HETO_CONTRACT_ADDRESS = 'erd1qqqqqqqqqqqqqpgqltgyu0kcwkfth9vyquv9s4dgmrx550rjj9qs258v5r';
+// export const HETO2HETO_CONTRACT_ABI = 'abi/dice2dice.abi.json';
+// export const HETO2HETO_CONTRACT_NAME = 'BitxStake';
+// export const HETO_TOKEN_TICKER = 'HETO';
+// export const HETO_TOKEN_ID = 'HETO-663cf8';
+// export const HETO_TOKEN_DECIMALS = 0;
+// export const HETO_CLAIM_PERIOD_IN_DAYS = 7;
+
+// export const BTX2HETO_CONTRACT_ADDRESS = 'erd1qqqqqqqqqqqqqpgq7r9n9u389xr23vqyye8maaetcg2r886vj9qsj7sl4k';
+// export const BTX2HETO_CONTRACT_ABI = 'abi/btx2dice.abi.json';
+// export const BTX2HETO_CONTRACT_NAME = 'BitxStake';
+
+// //
+// export const VESTING_CONTRACT_ADDRESS = 'erd1qqqqqqqqqqqqqpgqq0pw2yqjjmazj75j8ly5tmm5udg5hfkej9qs657v62';
+// export const VESTING_CONTRACT_ABI_URL = 'abi/btx-vesting.abi.json';
+// export const VESTING_CONTRACT_NAME = 'BtxVesting';
+
+// export const EGLD_WRAPPER_SC_ADDRESS = 'erd1qqqqqqqqqqqqqpgq7ykazrzd905zvnlr88dpfw06677lxe9w0n4suz00uh';
+
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-export const ENVIRONMENT = 'devnet';
+export const ENVIRONMENT = 'testnet';
 
 export const dAppName = 'BitX Platform';
 export const SOCIAL_WEBSITE_URL = 'https://bitxfinance.online/';
@@ -170,8 +265,8 @@ export const BTX2HETO_CONTRACT_ABI = 'abi/btx2dice.abi.json';
 export const BTX2HETO_CONTRACT_NAME = 'BitxStake';
 
 //
-export const VESTING_CONTRACT_ADDRESS = 'erd1qqqqqqqqqqqqqpgqq0pw2yqjjmazj75j8ly5tmm5udg5hfkej9qs657v62';
+export const VESTING_CONTRACT_ADDRESS = 'erd1qqqqqqqqqqqqqpgq4pkaf8agrehrmtqdcgtjvnrfutat7nwdj9qs989rtr';
 export const VESTING_CONTRACT_ABI_URL = 'abi/btx-vesting.abi.json';
 export const VESTING_CONTRACT_NAME = 'BtxVesting';
 
-export const EGLD_WRAPPER_SC_ADDRESS = 'erd1qqqqqqqqqqqqqpgq7ykazrzd905zvnlr88dpfw06677lxe9w0n4suz00uh';
+export const EGLD_WRAPPER_SC_ADDRESS = 'erd1qqqqqqqqqqqqqpgqhjdqy6rqz3vdqsr3wydvyha9xvw5n75h0n4swc0wnt';
--- src/data.tsx
+++ src/data.tsx
@@ -1,17 +1,42 @@
+// // devnet
+// export const TOKENS = {
+//     "MEX-450e50": {
+//         identifier: "MEX-450e50",
+//         ticker: "MEX",
+//         decimals: 18,
+//         logo: "https://media.elrond.com/tokens/asset/MEX-455c57/logo.svg",
+//         unit_price_in_usd: 0.00019,
+//     },
+//     "BTX-9dc317": {
+//         identifier: "BTX-9dc317",
+//         ticker: "BTX",
+//         decimals: 18,
+//         logo: "https://media.elrond.com/tokens/asset/BTX-48d004/logo.png",
+//         unit_price_in_usd: 0.015,
+//     },
+// };
+
 // devnet
 export const TOKENS = {
-    "MEX-450e50": {
-        identifier: "MEX-450e50",
-        ticker: "MEX",
+    "ASH-76f082": {
+        identifier: "ASH-76f082",
+        ticker: "ASH",
         decimals: 18,
-        logo: "https://media.elrond.com/tokens/asset/MEX-455c57/logo.svg",
-        unit_price_in_usd: 0.00019,
+        logo: "https://testnet-media.elrond.com/tokens/asset/ASH-76f082/logo.png",
+        unit_price_in_usd: 0.0013,
     },
-    "BTX-9dc317": {
-        identifier: "BTX-9dc317",
-        ticker: "BTX",
+    "RISA-282a52": {
+        identifier: "RISA-282a52",
+        ticker: "RISA",
         decimals: 18,
-        logo: "https://media.elrond.com/tokens/asset/BTX-48d004/logo.png",
-        unit_price_in_usd: 0.015,
+        logo: "https://media.elrond.com/tokens/asset/RISA-c115c7/logo.png",
+        unit_price_in_usd: 0.042,
+    },
+    "USDC-780dd8": {
+        identifier: "USDC-780dd8",
+        ticker: "USDC",
+        decimals: 6,
+        logo: "https://media.elrond.com/tokens/asset/USDC-c76f1f/logo.png",
+        unit_price_in_usd: 1,
     },
 };
