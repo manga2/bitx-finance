f9d0cabd77943d74650c6d23269311bba39d6c8b
manga2 <codingexpert1001@gmail.com>
2022-05-08 23:04:20 -0700

    changed egld wrapper SC address

--- src/config.tsx
+++ src/config.tsx
@@ -87,7 +87,7 @@ export const VESTING_CONTRACT_ADDRESS = 'erd1qqqqqqqqqqqqqpgqxqnap2d90f0sj0v34a0
 export const VESTING_CONTRACT_ABI_URL = 'abi/btx-vesting.abi.json';
 export const VESTING_CONTRACT_NAME = 'BtxVesting';
 
-export const EGLD_WRAPPER_SC_ADDRESS = 'erd1ss6u80ruas2phpmr82r42xnkd6rxy40g9jl69frppl4qez9w2jpsqj8x97';
+export const EGLD_WRAPPER_SC_ADDRESS = 'erd1qqqqqqqqqqqqqpgqhe8t5jewej70zupmh44jurgn29psua5l2jps3ntjj3';
 
 // ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 // export const ENVIRONMENT = 'devnet';
